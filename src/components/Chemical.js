6b46c26a1b17439a1eb2ff5f1258c901ca1467c3
isaacrez <isaacrez20@gmail.com>
2022-03-30 10:38:48 -0500

    Add Chemical class

---
+++ src/components/Chemical.js
@@ -0,0 +1,21 @@
+import {chemicals} from './constants/antoines.ts';
+
+class Chemical {
+  constructor(name) {
+    this.name = name
+
+    let antoines = chemicals[name]
+    this._A = antoines[0]
+    this._B = antoines[1]
+    this._C = antoines[2]
+
+    this.boilingPt = this._calculateBoilingPoint()
+  }
+
+  _calculateBoilingPoint = () => this._B / (this._A - Math.log(760)) - this._C
+
+  getSatPressure = tempK => Math.exp(this._A - this._B / (this._C + tempK))
+
+  getName = () => this.name
+  getBoilingPt = () => this.boilingPt
+}
