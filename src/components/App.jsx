83bd638d0a923ee0d0631594e13469936a45b39c
isaacrez <isaacrez20@gmail.com>
2022-03-31 08:45:39 -0500

    Add Selector for trial functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,20 @@
+import React from 'react'
 import './styles/App.css'
 
+import Chemical from './models/Chemical'
+import Selector from './Selector.tsx'
+
 function App() {
+  const [chemName, setChemName] = React.useState("water")
+  const chemical = new Chemical(chemName)
+
+  const chemOptions = ["water", "n-butane", "benzene", "toluene"]
+
   return (
     <div className="App">
       <h1>Binary Separations</h1>
+      <p>{chemical.getName()} boils at {Math.round(chemical.getBoilingPt())}!</p>
+      <Selector value={chemName} setValue={setChemName} options={chemOptions} />
     </div>
   );
 }
