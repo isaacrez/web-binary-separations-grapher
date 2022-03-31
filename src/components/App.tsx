0a761d547bc2b7cfef39f675173a870ffdd36a49
isaacrez <isaacrez20@gmail.com>
2022-03-31 09:17:51 -0500

    Add exclusive selectors for two chemicals

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,16 +6,22 @@ import Chemical from './models/Chemical.ts'
 import Selector from './Selector.tsx'
 
 function App() {
-  const [chemName, setChemName] = React.useState("water")
-  const chemical = new Chemical(chemName)
+  const [lightName, setLightName] = React.useState("benzene")
+  const [heavyName, setHeavyName] = React.useState("toluene")
 
-  const chemOptions = ["water", "n-butane", "benzene", "toluene"]
+  const light = new Chemical(lightName)
+  const heavy = new Chemical(heavyName)
+
+  const chemicalNames = Object.keys(chemicals)
 
   return (
     <div className="App">
       <h1>Binary Separations</h1>
-      <p>{chemical.getName()} boils at {Math.round(chemical.getBoilingPt())}!</p>
-      <Selector value={chemName} setValue={setChemName} options={chemOptions} />
+      <p>{light.getName()} boils at {Math.round(light.getBoilingPt())}K!</p>
+      <Selector value={lightName} setValue={setLightName} options={chemicalNames.filter(n => n !== heavyName)} />
+
+      <p>{heavy.getName()} boils at {Math.round(heavy.getBoilingPt())}K!</p>
+      <Selector value={heavyName} setValue={setHeavyName} options={chemicalNames.filter(n => n !== lightName)} />
     </div>
   );
 }
